98cd5175806de4a9e955c5e3077570729bc365f1
Steve Kimoi <moenisteve@gmail.com>
2023-03-09 13:15:03 +0300

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const verifyContract = await ethers.getContractFactory("Verify");
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const deployedVerifyContract = await verifyContract.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await deployedVerifyContract.deployed();
 
-  await lock.deployed();
+  console.log("Verify Contract Address:", deployedVerifyContract.address);
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Sleeping.....");
+  await sleep(40000);
+
+  await hre.run("verify:verify", {
+    address: deployedVerifyContract.address,
+    constructorArguments: [],
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
